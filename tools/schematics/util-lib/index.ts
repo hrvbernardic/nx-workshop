7cb75a7bc141ee86af798280024d9adf74f34b8a
Hrvoje Bernardić <hrvbernardic@gmail.com>
2020-11-18 19:21:37 +0100

    create util schematic

--- tools/schematics/util-lib/index.ts
+++ tools/schematics/util-lib/index.ts
@@ -3,7 +3,10 @@ import { chain, externalSchematic, Rule } from '@angular-devkit/schematics';
 export default function (schema: any): Rule {
   return chain([
     externalSchematic('@nrwl/workspace', 'lib', {
-      name: schema.name,
+      name: `util-${schema.name}`,
+      directory: schema.directory,
+      linter: 'tslint',
+      tags: `type:util, scope:${schema.directory}`,
     }),
   ]);
 }
